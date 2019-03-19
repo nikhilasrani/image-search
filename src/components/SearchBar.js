770f433e89387a624e9a7947839e466b5bfb65e5
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-03-19 23:46:27 +0530

    made and defined searchbar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = {
+    term: ""
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              onChange={e => this.setState({ term: e.target.value })}
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-class App extends React.Component {
-  render() {
-    return <div>Weather App</div>;
-  }
-}
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 
